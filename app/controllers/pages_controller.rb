fef682849b11065e2d4e6042d1df273140a2691b
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-07-17 15:09:13 -0400

    Integrated skills via model generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
+    # Pull all blog posts from Blog model & save in @posts instance var
     @posts = Blog.all
+    @skills = Skill.all
   end
 
   def about
