f2c58b24451f13a92acfcf529edb54d70d8284c7
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-07-19 12:46:55 -0400

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+	# about, & contact are static pages (hence why they use just 'get' vs. 'resources')
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  # Chagne route from pages/about to just about. Point about route to about action in pages controller.
+  # controller goes on left of #; action on right
+  # Note: You can pass in anything as route name (eg, 'about-me' instead of 'about')
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Set root path (home page) to pages#home (pages controller, home action)
+  root to: 'pages#home'
 end
