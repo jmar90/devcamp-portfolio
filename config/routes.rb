dec3aa747d3a67a957e3558711c499213afa9d92
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-07-19 13:19:25 -0400

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	# about, & contact are static pages (hence why they use just 'get' vs. 'resources')
-  resources :portfolios
+	# Customize show route for portolios (want it to be /portfolio vs. /portfolios). Also assign it custom prefix portfolio_show
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id/', to: 'portfolios#show', as: 'portfolio_show'
 
   # Chagne route from pages/about to just about. Point about route to about action in pages controller.
   # controller goes on left of #; action on right
