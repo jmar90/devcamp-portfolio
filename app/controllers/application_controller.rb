aeadfd6451377cacd352ef84fa565f25f77184d2
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-07-24 12:19:07 -0400

    Added new parameters for user registration and editing account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # Run this before all other controller
+  # This will whitelist the name parameter for devise (currently,
+    # it is not whitelisted & is not being saved to db)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # Whitelist name for register route (devise refers to it as sign_up)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # Whitelist name for register edit route
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
